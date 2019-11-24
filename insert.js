dc70239cc0424c76dfd4667259d73be6e9dbfee7
jayasree <jayasree@nighthack.in>
2019-11-24 13:35:16 +0530

    added carousel

--- insert.js
+++ insert.js
@@ -5,32 +5,128 @@ if(products != undefined){
   var proObj = JSON.parse(products);
   var products = proObj.products;
   if (products != undefined){
-    page.style.marginTop = "47px";
-    searchBar.style.marginTop = "47px";
-    wrapper.style.marginTop = "80px";
-    var html = '<div class="navbar rel-pro"><div class="rp-txt">Related Products Available</div><div class="dropdown rp-dp"><button class="dropbtn">' + products.length + ' Products found<i class="fa fa-caret-down"></i></button><div class="dropdown-content"><ul class="rl-pro-list">'
+    page.style.marginTop = "120px";
+    searchBar.style.marginTop = "120px";
+    wrapper.style.marginTop = "180px";
+    var html = '<div class="container"><div class="row"><div class="MultiCarousel" data-items="1,3,5,6" data-slide="6" id="MultiCarousel"  data-interval="1000"><div class="MultiCarousel-inner">'
     for (var index in products) {
       var product = products[index];
-        html +=  "<li><div class='pro-list'><div class='pr-img'><img src='" + product.imageUrl + "'></div><div class='pr-name'>" + product.name + "<br/>Mfg: "+ product.manufacturer + "<br/>MRP: Rs." + product.price +"</div></div></li>"
+      html += '<div class="item"><div><b><p>' + product.name + '</p></b><p>' + product.manufacturer + '</p><p> MRP : ' + product.price + '</p></div></div>'
     };
-    html += '</ul></div></div><div class="rel-pro-cls"> ×</div></div>';
+    html += '</div><button class="btn btn-primary leftLst"><</button><button class="btn btn-primary rightLst">></button></div></div></div>'
     appendHtml(wrapper, html);
-    document.getElementsByClassName("rel-pro-cls")[0].addEventListener("click", closeNavbar);
   }
 }
 
 function appendHtml(el, str) {
   var div = document.createElement('div');
-  div.className = "rel-pro-navbar";
+  div.className = "navbar rel-pro";
   div.setAttribute('id', 'rel-pro-id');
   div.innerHTML = str;
   el.parentNode.insertBefore(div, el);
 }
 
-function closeNavbar(){
-  var navbar = document.getElementById("rel-pro-id");
-  navbar.style.display = "none";
-  page.style.marginTop = "0px";
-  searchBar.style.marginTop = "0px";
-  wrapper.style.marginTop = "0px";
-}
+
+$(document).ready(function () {
+  var itemsMainDiv = ('.MultiCarousel');
+  var itemsDiv = ('.MultiCarousel-inner');
+  var itemWidth = "";
+
+  $('.leftLst, .rightLst').click(function () {
+    var condition = $(this).hasClass("leftLst");
+    if (condition)
+        click(0, this);
+    else
+        click(1, this)
+  });
+
+  ResCarouselSize();
+
+  $(window).resize(function () {
+    ResCarouselSize();
+  });
+
+  //this function define the size of the items
+  function ResCarouselSize() {
+    var incno = 0;
+    var dataItems = ("data-items");
+    var itemClass = ('.item');
+    var id = 0;
+    var btnParentSb = '';
+    var itemsSplit = '';
+    var sampwidth = $(itemsMainDiv).width();
+    var bodyWidth = $('body').width();
+    $(itemsDiv).each(function () {
+      id = id + 1;
+      var itemNumbers = $(this).find(itemClass).length;
+      btnParentSb = $(this).parent().attr(dataItems);
+      itemsSplit = btnParentSb.split(',');
+      $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+      if (bodyWidth >= 1200) {
+          incno = itemsSplit[3];
+          itemWidth = sampwidth / incno;
+      }
+      else if (bodyWidth >= 992) {
+          incno = itemsSplit[2];
+          itemWidth = sampwidth / incno;
+      }
+      else if (bodyWidth >= 768) {
+          incno = itemsSplit[1];
+          itemWidth = sampwidth / incno;
+      }
+      else {
+          incno = itemsSplit[0];
+          itemWidth = sampwidth / incno;
+      }
+      $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+      $(this).find(itemClass).each(function () {
+          $(this).outerWidth(itemWidth);
+      });
+
+      $(".leftLst").addClass("over");
+      $(".rightLst").removeClass("over");
+
+    });
+  }
+
+
+  //this function used to move the items
+  function ResCarousel(e, el, s) {
+    var leftBtn = ('.leftLst');
+    var rightBtn = ('.rightLst');
+    var translateXval = '';
+    var divStyle = $(el + ' ' + itemsDiv).css('transform');
+    var values = divStyle.match(/-?[\d\.]+/g);
+    var xds = Math.abs(values[4]);
+    if (e == 0) {
+      translateXval = parseInt(xds) - parseInt(itemWidth * s);
+      $(el + ' ' + rightBtn).removeClass("over");
+
+      if (translateXval <= itemWidth / 2) {
+        translateXval = 0;
+        $(el + ' ' + leftBtn).addClass("over");
+      }
+    }
+    else if (e == 1) {
+      var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+      translateXval = parseInt(xds) + parseInt(itemWidth * s);
+      $(el + ' ' + leftBtn).removeClass("over");
+
+      if (translateXval >= itemsCondition - itemWidth / 2) {
+        translateXval = itemsCondition;
+        $(el + ' ' + rightBtn).addClass("over");
+      }
+    }
+    $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+  }
+
+  //It is used to get some elements from btn
+  function click(ell, ee) {
+    var Parent = "#" + $(ee).parent().attr("id");
+    var slide = $(Parent).attr("data-slide");
+    ResCarousel(ell, Parent, slide);
+  }
+
+});
